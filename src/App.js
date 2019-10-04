093180692b067deb9f789a83ad22a44f0a7b3252
snd <snd@iMac-snd.local>
2019-10-04 17:58:09 +0800

    add some styles

--- src/App.js
+++ src/App.js
@@ -1,6 +1,17 @@
 import React from 'react';
+import styled from 'styled-components';
 import TopStoriesContainer from './containers/TopStoriesContainer';
 
-const App = () => <TopStoriesContainer />;
+const PageWrapper = styled.div`
+  max-width: 1200px;
+  margin: 0 auto;
+  padding: 0 30px;
+`;
+
+const App = () => (
+  <PageWrapper>
+    <TopStoriesContainer />
+  </PageWrapper>
+);
 
 export default App;
--- src/components/Story.js
+++ src/components/Story.js
@@ -1,7 +1,17 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import React, { useState, useEffect } from 'react';
+import styled from 'styled-components';
 import propTypes from 'prop-types';
 import { getStory } from '../services/hackerNewsAPI';
 
+const StoryWrapper = styled.article`
+  border-bottom: 1px solid gray;
+  padding-bottom: 10px;
+
+  &:last-of-type {
+    border-bottom: none;
+  }
+`;
 
 const Story = ({ storyId }) => {
   const [story, setStory] = useState([]);
@@ -11,14 +21,14 @@ const Story = ({ storyId }) => {
   }, []);
 
   return (
-    <div>
+    <StoryWrapper>
       <p>
         <strong>{story.title}</strong>
       </p>
       <p>{story.by}</p>
+      <p>{story.time}</p>
       <a href={story.url}>{story.url}</a>
-      <p>{JSON.stringify(story)}</p>
-    </div>
+    </StoryWrapper>
   );
 };
 
